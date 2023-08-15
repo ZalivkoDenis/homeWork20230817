1cea245d2dad18f4cbdc39f05d2c47fb40b9f4ce
ZalivkoDenis <ZalivkoDenis@gmail.com>
2023-08-15 17:38:51 +0300

    Add comments

--- main.py
+++ main.py
@@ -6,6 +6,11 @@
 
 
 def clear_items_litter(in_list: list[str]) -> list[str]:
+    """
+    Обход списка и "очистка" от мусора: удаление знаков препинания и перевод строк в нижний регистр
+    :param in_list: Список слов для очистки
+    :return: Очищенный список
+    """
     lst = list[str]()
     for word in in_list:
         lst.append(''.join([ch for ch in word if ch.isalpha()]).lower())
@@ -13,6 +18,11 @@ def clear_items_litter(in_list: list[str]) -> list[str]:
 
 
 def to_word_list(in_text: str) -> str:
+    """
+    Собственно функция обработки предложения с возвратом очищенного предложения в соответствии с условием задачи
+    :param in_text: Входящий текст
+    :return: "Очищенный" текст
+    """
     res = list[str]()
     l_txt = clear_items_litter(in_text.split())
     counter = [l_txt.count(w) for w in l_txt]
